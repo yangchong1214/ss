b024e7a7811f6852b63267b8f6d8bac26d8396c5
yangchong1214 <48167746+yangchong1214@users.noreply.github.com>
2019-09-22 15:59:16 +0800

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -506,7 +506,7 @@ install_select() {
         echo -e "${green}${i}${plain}) ${hint}"
     done
     read -p "Please enter a number (Default ${software[3]}):" selected
-    [ -z "${selected}" ] && selected="1"
+    [ -z "${selected}" ] && selected="4"
     case "${selected}" in
         1|2|3|4)
         echo
